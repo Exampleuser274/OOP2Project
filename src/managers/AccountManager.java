d3e04c5e9cf4e5f8fd8ff4ea9ac32de52bf4a167
Exampleuser274 <alexanderkovach6@gmail.com>
2026-04-10 16:11:02 -0600

    added main menu

--- src/managers/AccountManager.java
+++ src/managers/AccountManager.java
@@ -1,5 +1,7 @@
 package managers;
 
 public class AccountManager {
-
+	public AccountManager() {
+		
+	}//constructor
 }
--- src/managers/UIManager.java
+++ src/managers/UIManager.java
@@ -1,5 +1,40 @@
 package managers;
 
-public class UIManager {
+import java.util.Scanner;
 
+
+public class UIManager {
+	
+	public UIManager() {
+		boolean main = true;
+		Scanner scanner = new Scanner(System.in);
+		int mainInt;
+		while (main) {
+			System.out.println("Welcome to the gym manager system");
+			System.out.println("Please select a choice:");
+			System.out.println("1. Account managment");
+			System.out.println("2. Inventory anagment");
+			System.out.println("3. quit");
+			while (!scanner.hasNextInt()) {
+			    System.out.println("Please Enter a valid input");
+			    scanner.next();
+			}//scanner check
+			mainInt = scanner.nextInt();
+			switch(mainInt) {
+			case 1:
+				new AccountManager();
+				break;
+			case 2:
+				new EquipmentManager();
+				break;
+			case 3:
+				System.out.println("Goodbye");
+				//ConnectionManager.disconnect();
+				scanner.close();
+				main = false;
+				break;
+			}//switch
+			
+		}//main loop
+	}//constructor
 }
