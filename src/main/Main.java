43d70cc025c81efb28e37610b27d16688beb1c9a
BryanTheGreat01 <bryanegbonwon@gmail.com>
2026-04-12 01:02:05 -0600

    main
    added some stuffs to help connect my code to the database. im having trouble finding a database from the internet tho

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,47 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	static Connection conn = null;
+	static Statement stmt = null;
+	
+	private static final String SERVER = "localhost";
+	private static final int PORT = 3306;
+	private static final String DATABASE = "";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "password";
+	public static void main(String[] args) {
+		
+	//connect to the database
+	connect();
+
+	}
+
+	public static void connect() {
+		final String DB_URL = String.format("jdbc:mariadb://%s:%d/%s?user=%s&password=%s", 
+				SERVER, PORT, DATABASE, USERNAME, PASSWORD);
+		
+		//System.out.println(DB_URL);
+		try {
+			conn = DriverManager.getConnection(DB_URL);
+			System.out.println("DB connection established");
+			stmt = conn.createStatement();
+			
+		}catch(SQLException e) {
+			System.out.println("Problem connecting to DB: " + e.getMessage());
+		}
+	}
+
+	public static void displayAll(){
+		String sql = "";
+		//im going to add the db name here.
 	}
 
 }
